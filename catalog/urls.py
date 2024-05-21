0fe6211af017a608e47707b5a886d06b397bd87e
Serhii Marynin <serhii.marynin@gmail.com>
2024-05-21 17:25:38 +0300

    Add cart paths

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,7 +6,10 @@ from catalog import views
 app_name = "catalog"
 
 urlpatterns = [
-    path("", views.product_list, name="product_list"),
-    path("<slug:category_slug>/", views.product_list, name="product_list_by_category"),
-    path("<int:pk>/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("catalog/", views.product_list, name="product_list"),
+    path("catalog/<slug:category_slug>/", views.product_list, name="product_list_by_category"),
+    path("catalog/<int:pk>/<slug:slug>/", views.product_detail, name="product_detail"),
+    path("cart/", views.cart_detail, name="cart_detail"),
+    path("cart/add/<int:product_id>", views.cart_add, name="cart-add"),
+    path("cart/remove/<int:product_id>", views.cart_remove, name="cart-remove"),
 ]
