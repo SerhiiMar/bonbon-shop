c9a6b26ce1d749ea8624a0307805901bbedf624c
Serhii Marynin <serhii.marynin@gmail.com>
2024-05-21 23:57:01 +0300

    Add Order and OrderItem models, add utils.py

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+
+from catalog.utils import product_directory_path
 
 
 class Category(models.Model):
@@ -24,7 +27,7 @@ class Product(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.CASCADE, related_name="products"
     )
-    image = models.ImageField(upload_to="products/%y/%m/%d", blank=True)
+    image = models.ImageField(upload_to=product_directory_path, blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     available = models.BooleanField(default=True)
@@ -39,3 +42,33 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse("catalog:product_detail", args=[self.pk, self.slug])
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="orders")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    paid = models.BooleanField(default=False)
+    products = models.ManyToManyField(Product, through="OrderItem", related_name="orders")
+
+    class Meta:
+        ordering = ("-created",)
+
+    def __str__(self):
+        return f"order {self.id}"
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="order_items")
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.pk
+
+    def get_cost(self):
+        return self.price * self.quantity
